710ad3ae905405d17d9768a880471c4eb5daae05
adr101mg González <adr101mg@hotmail.com>
2022-11-25 22:26:56 -0600

    tests for division

--- src/app/division/division.ts
+++ src/app/division/division.ts
@@ -1,5 +1,10 @@
-export function division(oper1, oper2) {  
+export function division(oper1, oper2) {
     // refactoring
-    let div = oper1 / oper2;
+    let div=0;
+    if (oper2==0){
+      return 0;
+    }else{
+       div = oper1 / oper2;
+    }
     return div;
 }
